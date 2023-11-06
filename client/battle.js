3c98ad0f56df95d1cea342cbd86c89b3e5b0beb4
Khider <Khider.Tariq1@gmail.com>
2023-11-06 17:10:57 +0000

    changes to checkAnswer array, uses randomQuestion once on startup

--- client/battle.js
+++ client/battle.js
@@ -1,3 +1,5 @@
+randomQuestion();
+
 function shuffleArray(array) { //randomises the answer array
     for (var i = array.length - 1; i > 0; i--) {
         var j = Math.floor(Math.random() * (i + 1));
@@ -9,46 +11,10 @@ function shuffleArray(array) { //randomises the answer array
 }
 
 
-// async function randomQuestion(e){ // calls for a random question
+async function randomQuestion(){ // calls for a random question
 
-//     const arr = []
-//     e.preventDefault()
-//     console.log("Hit")
-//     const questionData = await fetch(`http://localhost:3000/questions`);
-//     //const questions = await fetch(`https://hip-hip.onrender.com/questions/${randIdx}`)
-//     const questions = await questionData.json();
-//     //console.log(questions.length);
-//     const randIdx = Math.floor(Math.random()*(questions.length-0)) //generates a random id
-
-//     if (!arr.includes(randIdx)){
-//         arr.push(randIdx) //adds id to array
-//         const randQuestion = await fetch(`http://localhost:3000/questions/${randIdx}`) //fetches from api
-//         const finalRandomQuestion = await randQuestion.json();
-//         console.log(finalRandomQuestion)
-        
-//         const answerArray = []
-//         answerArray.push(finalRandomQuestion["correct"])   //add each question to an array
-//         answerArray.push(finalRandomQuestion["incorrect1"])
-//         answerArray.push(finalRandomQuestion["incorrect2"])
-//         answerArray.push(finalRandomQuestion["incorrect3"])
-
-//         console.log(answerArray);
-//         const randomisedAnswerArray = shuffleArray(answerArray)
-//         console.log(randomisedAnswerArray);
-
-//         answer1.textContent = randomisedAnswerArray[0];
-//         answer2.textContent = randomisedAnswerArray[1];
-//         answer3.textContent = randomisedAnswerArray[3];
-//         answer4.textContent = randomisedAnswerArray[4];
-//         //return answerArray["correct"];
-//         }
-// }
-
-const arr = []
-const answerArray = []
-let i = 1;
-async function CheckAnswers(e){
-    e.preventDefault()
+    const arr = []
+    console.log("Hit")
     const questionData = await fetch(`http://localhost:3000/questions`);
     //const questions = await fetch(`https://hip-hip.onrender.com/questions/${randIdx}`)
     const questions = await questionData.json();
@@ -56,12 +22,12 @@ async function CheckAnswers(e){
     const randIdx = Math.floor(Math.random()*(questions.length-0)) //generates a random id
 
     if (!arr.includes(randIdx)){
-        console.log("hit 5");
         arr.push(randIdx) //adds id to array
         const randQuestion = await fetch(`http://localhost:3000/questions/${randIdx}`) //fetches from api
         const finalRandomQuestion = await randQuestion.json();
         console.log(finalRandomQuestion)
         
+        const answerArray = []
         answerArray.push(finalRandomQuestion["correct"])   //add each question to an array
         answerArray.push(finalRandomQuestion["incorrect1"])
         answerArray.push(finalRandomQuestion["incorrect2"])
@@ -76,25 +42,59 @@ async function CheckAnswers(e){
         answer3.textContent = randomisedAnswerArray[3];
         answer4.textContent = randomisedAnswerArray[4];
         //return answerArray["correct"];
+        }
+}
 
-        answerArray.length = 0;
+const arr = []
+const answerArray = []
+let i = 1;
+async function CheckAnswers(e){
+    e.preventDefault()
+    const questionData = await fetch(`http://localhost:3000/questions`);
+    //const questions = await fetch(`https://hip-hip.onrender.com/questions/${randIdx}`)
+    const questions = await questionData.json();
+    //console.log(questions.length);
+    const randIdx = Math.floor(Math.random()*(questions.length-0)) //generates a random id
+
+    if (!arr.includes(randIdx)){
+        arr.push(randIdx) //adds id to array
+        const randQuestion = await fetch(`http://localhost:3000/questions/${randIdx}`) //fetches from api
+        const finalRandomQuestion = await randQuestion.json();
+        console.log(finalRandomQuestion)
+        
+        answerArray.push(finalRandomQuestion["correct"])   //add each question to an array
+        answerArray.push(finalRandomQuestion["incorrect1"])
+        answerArray.push(finalRandomQuestion["incorrect2"])
+        answerArray.push(finalRandomQuestion["incorrect3"])
+
+        const randomisedAnswerArray = shuffleArray(answerArray)
 
         questionNum.textContent = i;
         i++;
-        console.log();
         question.textContent = finalRandomQuestion["Question"]
-
-        console.log(e.target.outerText);
-        if (e.target.outerText === answerArray[0]){
+        console.log(e);
+        console.log(e.target.outerText + " TEST 1");
+        //console.log(answerArray[0] + " TEST 2");
+        if (e.target.outerText === answerArray["correct"]){
             console.log("correct")
         }else{
             console.log("incorrect")
         }
+
+        // setTimeout(() => {
+        //     answer1.textContent = randomisedAnswerArray[0],
+        //     answer2.textContent = randomisedAnswerArray[1],
+        //     answer3.textContent = randomisedAnswerArray[2],
+        //     answer4.textContent = randomisedAnswerArray[3]
+        // }, 500)
+
         }
         
         if(arr.length === questions.length){
             console.log("GAME DONE");
         }
+
+        answerArray.length = 0;
 }
 
 const answer1 = document.querySelector("#answer1")
