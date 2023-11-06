83278a50f2c9c18aa98f7ca01245e9f70a3ce603
Khider <Khider.Tariq1@gmail.com>
2023-11-06 11:32:58 +0000

    adds get requests for empty route, questions route, and questions/random route, adds a logger

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
-require("dotenv").config()
-
+//require("dotenv").config()
+const cors = require('cors');
+const logger = require("./logger");
 const express = require('express');
+
 const app = express();
 const port = 3000;
 
+app.use(logger);
+const questions = require("./API")
+
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send('Welcome to the history game!');
 });
+
+app.get('/questions', (req, res) => {
+  res.send(questions);
+})
+
+app.get('/questions/random', (req, res) => {
+  //const randIdx = 8;
+  const randIdx = Math.floor(Math.random()*(questions.length-0))
+  res.send(questions[randIdx]);
+})
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
---
+++ logger.js
@@ -0,0 +1,7 @@
+const logger = (req,res,next) => {
+
+    console.log(req.method, req.originalUrl);
+    next()
+}
+
+module.exports = logger;
