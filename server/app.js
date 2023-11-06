3eda146c049c67470c8667057f3239bca2bf0344
“yourGithubUsername” <your@emailadd.com>
2023-11-06 09:39:25 +0000

    modifies json to define server app , updates gitignore

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ require("dotenv").config()
 
 const express = require('express');
 const app = express();
-const port = process.env.PORT;
+const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
