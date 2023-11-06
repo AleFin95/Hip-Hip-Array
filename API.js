d854463f250e2b2647a426e25ae17806ab4d9229
Khider <Khider.Tariq1@gmail.com>
2023-11-06 11:20:31 +0000

    add commas between each object in api.js

--- API.js
+++ API.js
@@ -5,63 +5,63 @@ const questions = [
         "incorrect1": "incorrect answer",
         "incorrect2": "incorrect answer",
         "incorrect3": "incorrect answer"
-    }
+    },
     {
         "Question": "This is the second question",
         "correct": "correct answer",
         "incorrect1": "incorrect answer",
         "incorrect2": "incorrect answer",
         "incorrect3": "incorrect answer"
-    }
+    },
     {
         "Question": "This is the third question",
         "correct": "correct answer",
         "incorrect1": "incorrect answer",
         "incorrect2": "incorrect answer",
         "incorrect3": "incorrect answer"
-    }
+    },
     {
         "Question": "This is the fourth question",
         "correct": "correct answer",
         "incorrect1": "incorrect answer",
         "incorrect2": "incorrect answer",
         "incorrect3": "incorrect answer"
-    }
+    },
     {
         "Question": "This is the fifth question",
         "correct": "correct answer",
         "incorrect1": "incorrect answer",
         "incorrect2": "incorrect answer",
         "incorrect3": "incorrect answer"
-    }
+    },
     {
         "Question": "This is the sixth question",
         "correct": "correct answer",
         "incorrect1": "incorrect answer",
         "incorrect2": "incorrect answer",
         "incorrect3": "incorrect answer"
-    }
+    },
     {
         "Question": "This is the seventh question",
         "correct": "correct answer",
         "incorrect1": "incorrect answer",
         "incorrect2": "incorrect answer",
         "incorrect3": "incorrect answer"
-    }
+    },
     {
         "Question": "This is the eigth question",
         "correct": "correct answer",
         "incorrect1": "incorrect answer",
         "incorrect2": "incorrect answer",
         "incorrect3": "incorrect answer"
-    }
+    },
     {
         "Question": "This is the ninth question",
         "correct": "correct answer",
         "incorrect1": "incorrect answer",
         "incorrect2": "incorrect answer",
         "incorrect3": "incorrect answer"
-    }
+    },
     {
         "Question": "This is the tenth question",
         "correct": "correct answer",
