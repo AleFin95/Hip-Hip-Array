f9078856c62aaae1a6afdd815ddd5313243f6e4b
David <david@dmfraser.com>
2023-11-06 12:24:06 +0000

    adds morefunctionality to randomQuestions function

--- client/battle.js
+++ client/battle.js
@@ -1,6 +1,9 @@
+const questions = require("../API")
+
 const arr = []
 
-const whatever = randomQuestion(arr)
+
+const newArr = randomQuestion(arr)
 
 async function randomQuestion(arr){
     const randIdx = Math.floor(Math.random()*(questions.length-0))
@@ -8,7 +11,9 @@ async function randomQuestion(arr){
         if (randIdx != arr[i]){
             arr.append(arr[i])
             const chosen_question = await fetch(`https://hip-hip.onrender.com/questions/${randIdx}`)
+            console.log(chosen_question)
         }
     }
     return arr
 }
+console.log(newArr)
