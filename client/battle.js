6a7cc129cd05c28db82b02ccfb24fb341c7f733b
Khider <Khider.Tariq1@gmail.com>
2023-11-07 11:25:22 +0000

    adds more tests to battle.js, changes var to let in shuffleArray function, adds testing scripts

--- client/battle.js
+++ client/battle.js
@@ -1,9 +1,9 @@
 //randomQuestion();
 
 function shuffleArray(array) { //randomises the answer array
-    for (var i = array.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = array[i];
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let temp = array[i];
         array[i] = array[j];
         array[j] = temp;
     }
--- client/battle.test.js
+++ client/battle.test.js
@@ -1,2 +1,47 @@
 const each = require("jest-each").default;
 const { shuffleArray, CheckAnswers } = require("./battle");
+
+describe("shuffleArray", () => {
+    
+    it("Exists", () => {
+        expect(shuffleArray).toBeDefined();
+    })
+
+    it("Is a function", () => {
+        expect(shuffleArray instanceof Function).toEqual(true);
+    })
+
+    it('shuffles an array', () => {
+        const originalArray = [1, 2, 3, 4, 5];
+        const shuffledArray = shuffleArray(originalArray);
+    
+        // Check if the shuffled array has the same elements
+        expect(shuffledArray).toEqual(expect.arrayContaining(originalArray));
+    
+        // Check if the shuffled array is not the same as the original array
+        expect(shuffledArray).not.toEqual(originalArray);
+      });
+    
+      it('returns an array of the same length', () => {
+        const originalArray = [1, 2, 3, 4, 5];
+        const shuffledArray = shuffleArray(originalArray);
+    
+        expect(shuffledArray.length).toBe(originalArray.length);
+      });
+
+
+})
+
+describe("checkAnswers", () => {
+    
+    it("Exists", () => {
+        expect(CheckAnswers).toBeDefined();
+    })
+
+    it("Is a function", () => {
+        expect(CheckAnswers instanceof Function).toEqual(true);
+    })
+
+
+
+})
