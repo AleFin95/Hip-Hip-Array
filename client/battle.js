a203dbe9d153fcb097f7fb7c107a047c19429467
David <david@dmfraser.com>
2023-11-07 12:26:49 +0000

    adds functional health bars for both henry and student

--- client/battle.js
+++ client/battle.js
@@ -1,4 +1,39 @@
 //randomQuestion();
+// health tracking section 
+let Hval = 0
+let Sval = 0
+let difficulty = "medium" //sets and checks difficulty of fight
+if (difficulty === "easy"){ //if easy
+    Hval = 50 //sets damage multipliers
+    Sval = 10
+} else if (difficulty === "medium"){
+    Hval = 30
+    Sval = 20
+}else if (difficulty === "hard"){
+    Hval = 20
+    Sval = 30
+}else{
+    Hval = 10
+    Sval = 35
+}
+// // let currentHealthH = document.getElementById("henryHealth")
+// let currentHealthS = document.getElementById("studentHealth")
+let currentHealthH = document.getElementById("henryHealth")
+let currentHealthS = document.getElementById("studentHealth")
+// let currentHealthH.value = 100 //sets health for both student and henry as global variables
+// let currentHealthS.value = 100
+const setHenryHealth  = function(val){ //updates henry's health 
+     //gets value of henry health from html
+    currentHealthH.value = currentHealthH.value - val //updates health dependent on difficulty
+}
+const setStudentHealth  = function(val){ //same as above but for student
+    
+    currentHealthS.value = currentHealthS.value - val
+}
+function reduceHealth(){ //temporary button for reducting health 
+    setStudentHealth(Sval)
+    setHenryHealth(Hval)
+}
 
 function shuffleArray(array) { //randomises the answer array
     for (var i = array.length - 1; i > 0; i--) {
@@ -58,35 +93,39 @@ async function CheckAnswers(e){
 
     const chosenAnswer = e.target.outerText;
 
-    if(arr.length === questions.length){
-        console.log("GAME DONE");
-    } 
-    else if(arr.includes(randIdx)){
+    if(currentHealthH.value < 1){ //checks henrys health 
+        console.log("Congratulations you beat Henry");
+    } else if (currentHealthS.value < 1){
+        console.log("you suck at this")
+    }
+    else if(arr.includes(randIdx)){ //checks players health
         CheckAnswers(e);
     }
     else {
         arr.push(randIdx) //adds id to array
         const randQuestion = await fetch(`http://localhost:3000/questions/${randIdx}`) //fetches from api
-        const finalRandomQuestion = await randQuestion.json();
+        const finalRandomQuestion = await randQuestion.json(); //converts to json
         console.log(finalRandomQuestion)
         
-        const correctAnswer = finalRandomQuestion["correct"]
+        const correctAnswer = finalRandomQuestion["correct"] //identifies correct anser 
 
         answerArray.push(finalRandomQuestion["correct"])   //add each question to an array
         answerArray.push(finalRandomQuestion["incorrect1"])
         answerArray.push(finalRandomQuestion["incorrect2"])
         answerArray.push(finalRandomQuestion["incorrect3"])
 
-        const randomisedAnswerArray = shuffleArray(answerArray)
+        const randomisedAnswerArray = shuffleArray(answerArray) //randomise question order
 
-        questionNum.textContent = i;
+        questionNum.textContent = (`Question ${i}`); //counts and increments question number
         i++;
-        question.textContent = finalRandomQuestion["Question"] 
+        question.textContent = finalRandomQuestion["Question"]  //sets question in html
 
-        if (chosenAnswer === correctAnswer){
+        if (chosenAnswer === correctAnswer){ //checks if chosen answer is correct or not 
             console.log("correct")
+            setHenryHealth(Hval)
         }else{
             console.log("incorrect")
+            setStudentHealth(Sval)
         }
 
         answer1.textContent = randomisedAnswerArray[0];
