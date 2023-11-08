030fb9b8ec7abd514c69bde8b5a0403d67f7e148
KengiLuca <kengiluca@gmail.com>
2023-11-08 12:29:48 +0000

    Fixed battle functionality

--- client/battle.js
+++ client/battle.js
@@ -53,78 +53,116 @@ function reduceHealth(){ //temporary button for reducting health
 }
 
 function shuffleArray(array) { //randomises the answer array
-    for (let i = array.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1));
-        let temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
-    return array // returns randomised answers
+     for (let i = array.length - 1; i > 0; i--) {
+         let j = Math.floor(Math.random() * (i + 1));
+         let temp = array[i];
+         array[i] = array[j];
+         array[j] = temp;
+     }
+//     return array // returns randomised answers
 }
 
+const questions = [];
 
+async function loadQuestions() {
+    try {
+      const questionData = await fetch("http://localhost:3000/questions");
+      if (!questionData.ok) {
+        throw new Error("Failed to fetch questions");
+      }
+      const data = await questionData.json();
+      if (Array.isArray(data)) {
+        questions.push(...data);
+      } else {
+        throw new Error("Invalid data received from the API");
+      }
+    } catch (error) {
+      console.error("Error loading questions:", error);
+    }
+  }
 
+let i = 1 
+const answerOptions = [];
+let selectedQuestion = null
+
+async function loadNextQuestion() {
+    if (questions.length === 0) {
+      // If no questions are loaded, load some questions
+      await loadQuestions();
+    }
+  
+    const randIdx = Math.floor(Math.random() * questions.length);
+    selectedQuestion = questions.splice(randIdx, 1)[0]; // Define selectedQuestion here
+  
+    questionNum.textContent = `Question ${i}`;
+    i++;
+    question.textContent = selectedQuestion["Question"];
+  
+    answerOptions.length = 0;
+    answerOptions.push(
+      selectedQuestion["correct"],
+      selectedQuestion["incorrect1"],
+      selectedQuestion["incorrect2"],
+      selectedQuestion["incorrect3"]
+    );
+  
+    shuffleArray(answerOptions); // Shuffle the answer options
+  
+    // Set the answer options for the current question
+    answer1.textContent = answerOptions[0];
+    answer2.textContent = answerOptions[1];
+    answer3.textContent = answerOptions[2];
+    answer4.textContent = answerOptions[3];
+  }
+  
 const arr = []
 const answerArray = []
-let i = 1;
-async function CheckAnswers(e){
-    e.preventDefault()
-    const questionData = await fetch(`http://localhost:3000/questions`);
-    //const questions = await fetch(`https://hip-hip.onrender.com/questions/${randIdx}`)
-    const questions = await questionData.json();
-    //console.log(questions.length);
-    const randIdx = Math.floor(Math.random()*(questions.length-0)) //generates a random id
-
-    const chosenAnswer = e.target.outerText;
+let isAnswerSubmitted = false
+let isFetchingQuestion = false;
 
 
-    if(arr.includes(randIdx)){ //checks players health
-        CheckAnswers(e);
+async function CheckAnswers(e) {
+    e.preventDefault();
+  
+    if (selectedQuestion === null) {
+      console.error("No question loaded");
+      return;
     }
-    else {
-        arr.push(randIdx) //adds id to array
-        const randQuestion = await fetch(`http://localhost:3000/questions/${randIdx}`) //fetches from api
-        const finalRandomQuestion = await randQuestion.json(); //converts to json
-        console.log(finalRandomQuestion)
-        
-        const correctAnswer = finalRandomQuestion["correct"] //identifies correct anser 
+  
+    const chosenAnswer = e.target.textContent;
+  
+    if (chosenAnswer === selectedQuestion["correct"]) {
+      console.log("Correct");
+      setHenryHealth(Hval);
+    } else {
+      console.log("Incorrect");
+      setStudentHealth(Sval);
+    }
+  
+    if (currentHealthH.value < 1) {
+      console.log("Congratulations, you beat Henry");
+    } else if (currentHealthS.value < 1) {
+      console.log("You suck at this");
+    }
+  
+    // Load the next question
+    await loadNextQuestion();
+  }
+  
+  // Add an initial call to load the first question
+  selectedQuestion = null;  // Initialize selectedQuestion to null
+  loadNextQuestion();
+  
+  // ... (rest of the code)
+  
+  
+  
+  
+  
+  
 
-        answerArray.push(finalRandomQuestion["correct"])   //add each question to an array
-        answerArray.push(finalRandomQuestion["incorrect1"])
-        answerArray.push(finalRandomQuestion["incorrect2"])
-        answerArray.push(finalRandomQuestion["incorrect3"])
-
-        const randomisedAnswerArray = shuffleArray(answerArray) //randomise question order
-
-        questionNum.textContent = (`Question ${i}`); //counts and increments question number
-        i++;
-        question.textContent = finalRandomQuestion["Question"]  //sets question in html
-        console.log(chosenAnswer + "hit")
-        console.log(correctAnswer + "HIT2")
-        if (chosenAnswer === correctAnswer){ //checks if chosen answer is correct or not 
-            console.log("correct")
-            setHenryHealth(Hval)
-        }else{
-            console.log("incorrect")
-            setStudentHealth(Sval)
-        }
-        if(currentHealthH.value < 1){ //checks henrys health 
-            console.log("Congratulations you beat Henry");
-        } else if (currentHealthS.value < 1){
-            console.log("you suck at this")
-        }
-
-        answer1.textContent = randomisedAnswerArray[0];
-        answer2.textContent = randomisedAnswerArray[1];
-        answer3.textContent = randomisedAnswerArray[2];
-        answer4.textContent = randomisedAnswerArray[3];
-        answerArray.length = 0;
-
-    } 
-}
 
 const answer1 = document.querySelector("#answer1")
-//const array = randomQuestion();
 answer1.addEventListener("click", CheckAnswers)
 
 const answer2 = document.querySelector("#answer2")
